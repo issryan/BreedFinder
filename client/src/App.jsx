95373ef3c0a3f92150a0f6ab89fa341197a1c036
Ryan <90571282+issryan@users.noreply.github.com>
2024-04-15 01:20:54 -0400

    added styling to frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -17,15 +17,13 @@ function App() {
 
   const fetchHistory = async () => {
     try {
-        const response = await axios.get('http://localhost:9000/history');
-        console.log("Fetched history:", response.data);  // Check what is received
-        setHistory(response.data);
+      const response = await axios.get('http://localhost:9000/history');
+      setHistory(response.data);
     } catch (error) {
-        console.error('Error fetching history:', error);
-        setError('Failed to fetch history');
+      console.error('Error fetching history:', error);
+      setError('Failed to fetch history');
     }
-};
-
+  };
 
   const handleFileChange = (event) => {
     const file = event.target.files[0];
@@ -78,26 +76,31 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Dog Breed Classifier</h1>
-        <form onSubmit={handleSubmit}>
-          <input type="file" className="custom-file-input" onChange={handleFileChange} />
-          {imagePreviewUrl && <img src={imagePreviewUrl} alt="Dog Preview" />}
-          <button type="submit">Classify Breed</button>
-        </form>
-        {isLoading && <p>Loading...</p>}
-        {breed && <p>{breed}</p>}
-        {confidence && <p>{confidence}</p>}
-        {error && <p className="error">{error}</p>}
-        <h2>Prediction History</h2>
-        <div>
-          {history.map((entry, index) => (
-            <div key={index}>
-              <p>{entry.timestamp}: {entry.breed_name} - Confidence: {entry.confidence}%</p>
-            </div>
-          ))}
+      <div className="container">
+        <div className="section">
+          <h1>Breed Finder</h1>
+          <p>Welcome to the Dog Breed Classifier! Please upload an image of a dog, and our AI will predict its breed.</p>
+          <form onSubmit={handleSubmit}>
+            <input type="file" className="custom-file-input" onChange={handleFileChange} />
+            {imagePreviewUrl && <img src={imagePreviewUrl} alt="Dog Preview" />}
+            <button type="submit">Predict</button>
+          </form>
+          {isLoading && <p>Loading...</p>}
+          {breed && <p>{breed}</p>}
+          {confidence && <p>{confidence}</p>}
+          {error && <p className="error">{error}</p>}
         </div>
-      </header>
+        <div className="section">
+          <h2>Prediction History</h2>
+          <div>
+            {history.map((entry, index) => (
+              <div key={index}>
+                <p>{entry.timestamp}: {entry.breed_name} - Confidence: {entry.confidence}%</p>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
